f5cbe449ac94b5bd70b73d4c25a4d30b96cd4029
sachint97 <sachint94482@gmail.com>
2022-06-29 19:38:29 +0530

    environmental variables postgresql

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -74,13 +74,23 @@ WSGI_APPLICATION = 'todo_list.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('todo_db_name'),
+        'USER': os.environ.get('todo_db_user'),
+        'PASSWORD': os.environ.get('todo_db_pass'),
+        'HOST': os.environ.get('todo_db_host'),
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
